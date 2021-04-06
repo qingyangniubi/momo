550814e3abb64b488d4d200493cb1bf25b9ef4dd
钱棋 <1828563934@qq.com>
2021-04-06 21:22:34 +0800

    创建文件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入element ui
+import ElementUi from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUi)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <!-- 首页 -->
+  <el-container>
+    <!-- 头部 -->
+    <el-header>
+      <div class="header-box">
+        <el-row>
+          <el-col :span="5">
+            <img
+              class="logo"
+              src="../assets/img/20771839_1609920594.png"
+              alt=""
+            />
+          </el-col>
+
+          <el-col :span="19">
+            <ul class="nav">
+              <li>首页</li>
+              <li>关于我们</li>
+              <li>模特展示</li>
+              <li>环境展示</li>
+              <li>夜场新闻</li>
+              <li>联系我们</li>
+              <li>招聘要求</li>
+              <li class="li-tel">
+                <img src="../assets/img/7877635_1538278040.png" alt="" />
+                <span>13688143752</span>
+              </li>
+            </ul>
+          </el-col>
+        </el-row>
+      </div>
+    </el-header>
+    <!-- 身体 -->
+    <el-main>Main</el-main>
+    <!-- 尾部 -->
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import "../assets/css/common.css";
+</style>
